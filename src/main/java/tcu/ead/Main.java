deafbf6f9c64fe9984b7fccbe3fa187e38b95f51
fangxingchaochao <1505457259@qq.com>
2023-06-06 09:01:20 +0800

    init project

---
+++ src/main/java/tcu/ead/Main.java
@@ -0,0 +1,11 @@
+package tcu.ead;
+
+/**
+ * @Author fangxingchao
+ * @Date ${YEAR}/${MONTH}/${DAY}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
